4883e78065eda36da31c9cc9bf37a14eb289bdd1
Bryan Biedenkapp <gatekeep@gmail.com>
2026-07-01 16:35:49 -0400

    report IP and port for oversized packet warning;

--- src/common/network/FrameQueue.cpp
+++ src/common/network/FrameQueue.cpp
@@ -157,7 +157,8 @@ bool FrameQueue::write(const uint8_t* message, uint32_t length, uint32_t streamI
     // bryanb: this is really a developer warning not a end-user warning, there's nothing the end-users can do about
     //  this message
     if (bufferLen > (DATA_PACKET_LENGTH - OVERSIZED_PACKET_WARN)) {
-        LogDebug(LOG_NET, "FrameQueue::write(), WARN: packet length is possibly oversized, possible data truncation - BUGBUG");
+        LogWarning(LOG_NET, "FrameQueue::enqueueMessage(), WARN: packet length is possibly oversized, possible data truncation - BUGBUG, address = %s, port = %u", 
+            udp::Socket::address(addr).c_str(), udp::Socket::port(addr));
     }
 
     bool ret = true;
